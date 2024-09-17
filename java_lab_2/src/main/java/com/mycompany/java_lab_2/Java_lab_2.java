406bc0204d69675fd73ba1b68896b23a22233a64
Sergey Ermakov <ermakoffsergey@gmail.com>
2024-09-17 12:56:25 +0300

    add main java file

---
+++ java_lab_2/src/main/java/com/mycompany/java_lab_2/Java_lab_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_lab_2;
+
+/**
+ *
+ * @author ermak
+ */
+public class Java_lab_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
