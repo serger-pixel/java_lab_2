ca2019283390fc7245a39e93396a0033f985af7a
Vlad_P <vlad.petrovskiy00@mail.ru>
2024-09-21 15:10:42 +0300

    Lecture

--- java_lab_2/src/main/java/Classes/Password.java
+++ java_lab_2/src/main/java/Classes/Password.java
@@ -1,20 +1,33 @@
 package Classes;
 import java.util.regex.Pattern;
-
+import java.util.regex.Matcher;
 /**
  *
  * @author ermak
  */
 public class Password {
     private String _value;
-    private String _reg = "[A-Z+a-z+0-9]{8,}";
-    private Pattern _temp;
+    private static final String _reg = "([A-Z]+)([a-z]+)([0-9]+)(_*){8,}";
     
     public Password(String value){
-        _value = value;
+       // _value = value;
+        Pattern temp = Pattern.compile(_reg);
+        //_matcher = temp.matcher(value);
+        setPassword()
     }
     
     public Password(){
+        
+    }
+    
+    public boolean setPassword(String value){
+        Pattern temp = Pattern.compile(_reg);
+        Matcher matcher = temp.matcher(value);
+        if (!matcher.matches()){
+            
+            setPassword();
+        }
+        _value = value;
         
     }
 }
